dffd344cbda158810c96478843850d71845c4540
Eija Werner <github@eija.se>
2022-09-09 15:46:43 +0200

    Progress component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,55 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--background-radius": "4px"
+  },
+  medium: {
+    "--height": "12px",
+    "--background-radius": "4px"
+  },
+  large: {
+    "--padding": "2px",
+    "--height": "24px",
+    "--background-radius": "8px"
+  }
+}
+const ProgressBar = ({ value, size, ariaLabel }) => {
+  const styles = SIZES[size];
+  return <>
+    <Wrapper role="progressbar"
+          aria-label={ariaLabel}
+          aria-valuenow={value}
+          style={styles}
+    >
+      <Bar value={value}/>
+    </Wrapper>
+    </>;
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  
+  height: var(--height);
+  padding: var(--padding);
+  
+  border-radius: var(--background-radius);
+  background: ${COLORS.transparentGray15};
+  
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const Bar = styled.div`
+    background: ${COLORS.primary};
+    width: ${p => p.value}%;
+    height: 100%;
+    
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border-top-right-radius: ${p => p.value/100 * 4}px;
+    border-bottom-right-radius: ${p => p.value/100 * 4}px;
+`;
 
 export default ProgressBar;
